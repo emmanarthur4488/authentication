bb68bbc3528256ca8a002ad991620425620f9427
emmanarthur4488 <arthurgodswill56@gmail.com>
2024-01-16 13:59:07 -0800

    first commit

---
+++ react-class-3/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import About from './Components/About/About'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <About/>
+    </>
+  )
+}
+
+export default App
